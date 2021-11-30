fb37ec437c10c7df07b7742a3d03f09d692377b1
Nathan Fritch <nbfritch@gmail.com>
2021-11-30 17:05:49 -0600

    post index/view

--- posts.ts
+++ posts.ts
@@ -1,11 +1,39 @@
 import { helpers, Router } from "./deps.ts";
 import type { Context } from "./deps.ts";
+import { renderView } from "./viewHelper.ts";
+import { getDb } from "./database.ts";
 
 const postRouter = new Router();
 
-postRouter.get("/", (context: Context) => {
-  // TODO
-  context.response.body = "post index";
+interface Post {
+  id: number,
+  title: string,
+  body: string,
+  boardid: number,
+  name: string
+}
+
+postRouter.get("/", async (context: Context) => {
+  const db = getDb();
+
+  const posts = await db.queryObject<Post>`
+    Select
+      p.id,
+      p.title,
+      p.body,
+      p.boardid,
+      b.name
+    From Posts p
+    Join Boards b
+      On p.boardid = b.id
+    Order By p.id Desc
+    Limit 25;
+  `;
+
+  renderView(context, "post_index.eta", {
+    title: "Latest",
+    posts: posts.rows
+  });
 });
 
 postRouter.get("/new", (context: Context) => {
@@ -18,11 +46,42 @@ postRouter.post("/new", (context: Context) => {
   context.response.body = "post created";
 });
 
-postRouter.get("/:post-id", (context: Context) => {
-  // TODO
-  const { requestedPost } = helpers.getQuery(context, { mergeParams: true });
+postRouter.get("/:postid", async (context: Context) => {
+  const { postid } = helpers.getQuery(context, {mergeParams: true});
 
-  context.response.body = `view post ${requestedPost ?? "unknown"}`;
+  if (!postid) {
+    renderView(context, "not_found.eta", {
+      title: "Post not found"
+    })
+  }
+
+  const db = getDb();
+
+  const post = await db.queryObject<Post>`
+    Select
+      p.id,
+      p.title,
+      p.body,
+      p.boardid,
+      b.name
+    From Posts p
+    Join Boards b
+      On p.boardid = b.id
+    Where p.id = ${postid}
+    Limit 1;
+  `;
+
+  if (post.rows.length) {
+    const p = post.rows[0];
+    renderView(context, "post_view.eta", {
+      title: p.title,
+      post: p
+    });
+  } else {
+    renderView(context, "not_found.eta", {
+      title: "Post not found"
+    });
+  }
 });
 
 export { postRouter };
